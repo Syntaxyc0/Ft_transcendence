71d2a92205be0039bb0c0464c47c65ed6a5c3509
Syntaxyc0 <jeanbesnier@yahoo.fr>
2023-10-10 16:58:02 +0200

    angular env

--- Frontend/src/app/landing-page/landing-page.component.ts
+++ Frontend/src/app/landing-page/landing-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-landing-page',
@@ -14,5 +15,5 @@ export class LandingPageComponent {
 
 	constructor() {}
 	code: any
-	redirect_url = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-000e4b8f9307f65844fe94cf2de9ad19e124143666cadeb78d8a1b7755a42b3f&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fredirect&response_type=code'
+	redirect_url = environment.redirect_url
 }
---
+++ Frontend/src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+	redirect_url:'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-000e4b8f9307f65844fe94cf2de9ad19e124143666cadeb78d8a1b7755a42b3f&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fredirect&response_type=code',
+	
+
+  };
