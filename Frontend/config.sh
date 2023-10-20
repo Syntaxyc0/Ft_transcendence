3f01de7f28fa6fceec94c4fff2860354e1acb23c
Elise Colin <ecolin@student.42.fr>
2023-10-20 13:43:57 +0200

    fix version

--- Frontend/config.sh
+++ Frontend/config.sh
@@ -2,6 +2,6 @@
 chown -R root:root /app/Frontend
 chmod -R 755 /app/Frontend
 cd /app/Frontend
-npm install
-npm install --legacy-peer-deps
+ncu -u
+npm install --force --legacy-peer-deps
 ng serve --host 0.0.0.0
