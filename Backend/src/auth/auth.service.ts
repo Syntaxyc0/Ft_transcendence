25a52a48400d2fcb5c55274120c2342ec2a0f294
Syntaxyc0 <jeanbesnier@yahoo.fr>
2023-10-31 11:21:16 +0100

    twofa page

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -83,7 +83,7 @@ export class AuthService
                 login : login,
             },
         });
-        if (alreadyregistered.is2faenabled)
+        if (alreadyregistered)
         {
 
         }
