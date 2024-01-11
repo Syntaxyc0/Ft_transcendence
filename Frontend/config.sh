63ff482034aca3b6aaf13e739198aaa6aa994827
Syntaxyc0 <jeanbesnier@yahoo.fr>
2024-01-11 11:40:11 +0100

    remove error when relaunch server + make install silent

--- Frontend/config.sh
+++ Frontend/config.sh
@@ -2,5 +2,5 @@
 chown -R root:root /app/Frontend
 chmod -R 755 /app/Frontend
 cd /app/Frontend
-npm install --force --legacy-peer-deps
+npm install --force --legacy-peer-deps --silent
 ng serve --host 0.0.0.0 --disable-host-check
